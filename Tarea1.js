4a9c172717b124dcd0a16e54c10885c2e08ac8a8
Miguel Mosquera <miguelangelmosb@gmail.com>
2020-07-27 13:17:31 -0500

    update

---
+++ Tarea1.js
@@ -0,0 +1,24 @@
+const padre = {
+    nombre : 'Rafael',
+    apellido : ' Mosquera',
+    edad : 52,
+    sexo : 'Masculino'
+  }
+  
+  const gato = {
+    nombre : 'Bagheera',
+    apellido : ' Mosquera',
+    edad : 1,
+    sexo : 'Masculino'
+  }
+  
+  const yo = {
+    nombre : 'Miguel',
+    apellido : ' Mosquera',
+    edad : 22,
+    sexo : 'Masculino'
+  }
+  
+  const arrayFinal = [padre, gato, yo]
+  
+  console.log(arrayFinal)
